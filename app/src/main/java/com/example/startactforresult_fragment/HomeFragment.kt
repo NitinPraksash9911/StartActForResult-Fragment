2f632687ee5429927b98a9d69ef69acea51c448c
Nitin Prakash <32475878+NitinPraksash9911@users.noreply.github.com>
2023-01-11 19:53:08 +0530

    added biometeric api (#2)

--- app/src/main/java/com/example/startactforresult_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/startactforresult_fragment/HomeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.Fragment
 import com.example.startactforresult_fragment.databinding.FragmentHomeBinding
+import com.example.startactforresult_fragment.util.BiometricAuthUtil
 import com.example.startactforresult_fragment.yes_bank.FetchYesBankDeviceToken
 import com.example.startactforresult_fragment.yes_bank.YesBankDataWrapper
 
@@ -14,13 +17,18 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
+    lateinit var biometricAuthUtil: BiometricAuthUtil
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        biometricAuthUtil = BiometricAuthUtil(requireActivity())
+        biometricPrompt = biometricAuthUtil.createBiometricPrompt()
+        promptInfo = biometricAuthUtil.generatePromptInfo()
         return binding.root
     }
 
@@ -29,8 +37,17 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btn.setOnClickListener {
-            fetchArbitraryData.launch(YesBankDataWrapper(count))
-            count++
+//            fetchArbitraryData.launch(YesBankDataWrapper(count))
+//            count++
+            checkBoiMetric()
+        }
+    }
+
+    fun checkBoiMetric(){
+        if(biometricAuthUtil.isBiometricAvailable()){
+            biometricPrompt.authenticate(promptInfo)
+        }else{
+            Toast.makeText(requireActivity().applicationContext,"Biometric not available",Toast.LENGTH_LONG).show()
         }
     }
 
