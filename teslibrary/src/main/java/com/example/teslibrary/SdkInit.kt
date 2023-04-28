431535c4e6cc5d9c8d79490d15015f2fd3f2ffb1
Nitin Prakash <nitin@upswing.one>
2023-04-29 01:39:21 +0530

    renamed hello

--- teslibrary/src/main/java/com/example/teslibrary/SdkInit.kt
+++ teslibrary/src/main/java/com/example/teslibrary/SdkInit.kt
@@ -7,6 +7,6 @@ object SdkInit {
 
     @JvmStatic
     fun initSdk(context: Context) {
-        Toast.makeText(context, "Android test liaad basdasdarary", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "Helloo", Toast.LENGTH_SHORT).show()
     }
 }
